5cbede9f8811864de07a9bf505c5f2cc3ad1939c
Naresh <naresh.kktr@gmail.com>
2023-04-20 12:58:37 +0530

    GetUser Api created

--- src/main/java/com/elite/controller/UserController.java
+++ src/main/java/com/elite/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.elite.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,17 @@ public class UserController {
     public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
     	UserEntity savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+	
+	@GetMapping
+    public ResponseEntity<List<UserEntity>> getUsers(){
+    	List<UserEntity> getUser = userService.getUsers();
+        return new ResponseEntity<>(getUser, HttpStatus.CREATED);
+    }
+	
+	@GetMapping("{id}")
+    public ResponseEntity<UserEntity> getUser(@PathVariable("id") Long userId){
+    	UserEntity getUser = userService.getUsers(userId);
+        return new ResponseEntity<>(getUser, HttpStatus.OK);
     }
 }
--- src/main/java/com/elite/service/UserService.java
+++ src/main/java/com/elite/service/UserService.java
@@ -1,5 +1,7 @@
 package com.elite.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,16 @@ public class UserService {
 	public UserEntity createUser(UserEntity user) {
 		// TODO Auto-generated method stub
 		return userRepository.save(user);
+	}
+
+	public List<UserEntity> getUsers() {
+		// TODO Auto-generated method stub
+		return userRepository.findAll();
+	}
+
+	public UserEntity getUsers(Long userId) {
+		// TODO Auto-generated method stub
+		return userRepository.findById(userId).get();
 	}
 
 }
